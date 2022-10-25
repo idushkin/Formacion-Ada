e94e1e8ed325186f7b8f2391aac81cacc6829f9e
Dushkin Dushkin <idushkin@indra.es>
2022-10-25 11:01:25 +0200

    Subida 25 Octubre 2

--- 09MasArreglos/src/floatarray.adb
+++ 09MasArreglos/src/floatarray.adb
@@ -1,33 +1,6 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.NUmerics.Float_Random; use Ada.Numerics.Float_Random;
-with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+package body FloatArray is
 
-procedure Main is
-   begin
-   --  declare
-   --  --type Montos is array (Integer range <>) of Float;
-   --  type Dinero is digits 2;
-   --  --Empieza en la posicion 1;
-   --  type Montos is array (Positive range <>) of Dinero;
-   --  --Empieza en la posicion 0;
-   --  type Array_Natural is array (Natural range <>) of Dinero;
-   --  --Empieze en la posicion Integer'First
-   --  type Array_integer is array (Integer range <>) of Dinero;
-   --
-   --  --Ventas : Montos(1..5);
-   --  Ventas : Montos := (10.0, 2.3, 6.7);
-   --  Ventas_Natural_Range : Array_Natural := (4.5,3.4);
-   --  Ventas_Integer_Range : Array_Integer := (6.7, 2.3);
-   --  begin
-   --     -- Insert code here
-   --     --Ventas(0) := 2.3; --Porque da error?
-   --  Put_Line("Primera Posicion (Positive range <>) " & Ventas'First'Image );
-   --  Put_Line("Primera Posicion (Natural range <>) " & Ventas_Natural_Range'First'Image);
-   --  Put_Line("Primera Posicion (Integer range <>) " & Ventas_Integer_Range'First'Image);
-   --     end;
-
-   declare
-      --declarar un tipo de Arreglo de Float (abierto/unconstrained)
+    --declarar un tipo de Arreglo de Float (abierto/unconstrained)
       type Array_Float is Array(Positive range <>) of Float;
 
       --declarar una funcion que recibe un numero y me devuelve un arreglo
@@ -69,7 +42,7 @@ procedure Main is
             Put_Line("");
          end loop;
       end Mostrar;
-
+      
       --Mayores
       --Una function que reciba un array y un valor
       --Devuelve otro array con los valores mayores al parametro
@@ -78,42 +51,20 @@ procedure Main is
          Aux : Array_Float := Arreglo; --Copio el arreglo para que tenga la misma longitud
          Indice : Integer := Arreglo'First;
       begin
-
+         
          for I in Arreglo'Range loop
             if Arreglo(I)>Valor then
                Aux(Indice) := Arreglo(I);
                Indice := Indice+1;
             end if;
          end loop;
-
+         
          declare
             Result : Array_Float(Arreglo'First..Indice-1); --Me puede quedar vacio
          begin
             Result := Aux(Arreglo'First..Indice-1); --Slice
-            return Result;
+            return Result;           
          end;
       end Mayores;
 
-      Numeros : Array_Float:=Cargar_Arreglo(10);
-   begin
-      --  for I in reverse Numeros'Range loop
-      --     Put(Numeros (I), Exp => 0);
-      --     Put_Line("");
-      --  end loop;
-      Mostrar(Numeros);
-      Put_Line("La Sumatoria es : ");
-      Put(Sumatoria(Numeros), Exp => 0);
-
-      Put_Line("");
-      Put_Line("El promedio es : ");
-      Put(Promedio(Numeros), Exp => 0);
-      Put_Line("");
-
-      declare
-         Mayores_A_5 : Array_Float := Mayores(Numeros,5.0);
-      begin
-         Put_Line("Los mayores a 5 son");
-         Mostrar(Mayores_A_5);
-      end;
-   end;
-end Main;
+end FloatArray;
